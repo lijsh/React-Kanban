4d1dcf95fb8e00411ce5d37b121430904aa5ccbd
lijiashen <sysulijs@gmail.com>
2016-03-06 21:08:58 +0800

    finish feature excluding dnd

---
+++ app/components/Notes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Note from './Note';
+import Editable from '../components/Editable'
+
+export default ({notes, onValueClick, onEdit, onDelete}) => {
+  return (
+    <ul className="notes">{notes.map(note =>
+      <li className="note" key={note.id}>
+        <Editable 
+          editing={note.editing}
+          value={note.task}
+          onValueClick={onValueClick.bind(null, note.id)}
+          onEdit={onEdit.bind(null, note.id)}
+          onDelete={onDelete.bind(null, note.id)} />
+      </li>
+    )}</ul>
+  )
+}
